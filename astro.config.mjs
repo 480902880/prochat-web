db8e2bf457bfce55bd9b5fe158dc339ab846c933
480902880 <480902880@qq.com>
2026-05-01 21:37:43 +0800

    complete responsive seo and mobile homepage updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: "https://prochat.cc",
+  output: "static",
   i18n: {
     defaultLocale: "zh",
     locales: ["zh", "zh-Hant", "en", "ja"],
